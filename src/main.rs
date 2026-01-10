ff429b9dab8eafb1cc33f6e006410a2739b07db2
Wilfred Hughes <me@wilfred.me.uk>
2026-01-10 22:48:04 +0000

    Improve error handling to print invalid response bodies

--- src/main.rs
+++ src/main.rs
@@ -140,7 +140,10 @@ async fn sync_issues_for_repo(user: &str, repo: &str, token: &str) -> Result<(),
         .send()
         .await?;
     
-    let github_issues: Vec<GitHubIssue> = response.json().await?;
+    let body = response.text().await?;
+    let github_issues: Vec<GitHubIssue> = serde_json::from_str(&body)
+        .map_err(|e| format!("Error decoding response: {}. Response body: {}", e, body))?;
+    
     let mut conn = establish_connection()?;
     
     for gh_issue in github_issues {
