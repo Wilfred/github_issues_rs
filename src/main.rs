e125baec263389d1daed81df87dad2921d69bbc0
Wilfred Hughes <me@wilfred.me.uk>
2026-01-10 22:21:38 +0000

    Add clap dependency and sync subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "github_issues_rs")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Sync command
+    Sync,
+}
+
 fn main() {
-    println!("Hello World");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Sync => {
+            println!("hello sync");
+        }
+    }
 }
